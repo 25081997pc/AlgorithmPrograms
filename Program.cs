00d21c4f91c7effbd61151e8af058c02b25bbb5e
25081997pc <chavanpooja2508@gmail.com>
2022-08-01 20:57:10 +0530

    Implementation of Insertion Sort Program

--- Program.cs
+++ Program.cs
@@ -2,9 +2,41 @@
 {
     class Program
     {
+        //Program for Implementation of Insertion Sort
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Algorithm Programs");
+            //Variables
+            int n = 5, i, j, val, flag;
+            int[] arr = new int[5] { 45, 3, 78, 11, 1 };
+            Console.WriteLine("Insertion Sort");
+            Console.WriteLine("Initial array is : ");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            for (i = 1; i < n; i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else
+                        flag = 1;
+                }
+            }
+            //Sorted Array List
+            Console.WriteLine("Sorted Array is :");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
         }
     }
 }
